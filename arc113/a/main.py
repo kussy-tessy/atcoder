cfbe367319e5bf7d611fff6a0e36f936078f406d
kussy <teppei04285000@gmail.com>
2021-03-24 06:54:03 +0900

    arc113

--- arc113/a/main.py
+++ arc113/a/main.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-from collection import defaultdict
-import math
+# from collection import defaultdict
+# import math
 
 K = int(input())
 
+ans = 0
+for A in range(1, 2*10**5+1):
+    for B in range(1, 2*10**5+1):
+        ans += K // (A*B)
+        if A * B >= K:
+           break 
+    if A > K:
+        break
+print(ans)
--- arc113/b/main.py
+++ arc113/b/main.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 
+N = int(input())
+Ss = []
+for _ in range(N):
+    Ss.append(input())
+
+    
